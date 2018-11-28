b656d16713153a5c743b77d3ff018c8d2a931513
Emma Loughlin <loughlem@bc.edu>
2018-11-27 19:41:55 -0500

    Initial Commit

---
+++ CollegeDecision/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollegeDecision
+//
+//  Created by Emma Loughlin on 11/27/18.
+//  Copyright © 2018 Emma Loughlin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
