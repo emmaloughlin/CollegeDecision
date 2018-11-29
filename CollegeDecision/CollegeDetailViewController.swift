ed8227f91e48db4490adefbeccd2d6e266d2168e
Emma Loughlin <loughlem@bc.edu>
2018-11-28 20:30:20 -0500

    CollegeDecision

---
+++ CollegeDecision/CollegeDetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CollegesListViewController.swift
+//  CollegeDecision
+//
+//  Created by Emma Loughlin on 11/28/18.
+//  Copyright © 2018 Emma Loughlin. All rights reserved.
+//
+
+import UIKit
+
+class CollegeDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
